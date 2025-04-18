933a7ee8dd3b784367b8a1d61bf5e4d8618f5b5f
dongha <donghak937@gmail.com>
2025-04-18 17:03:37 +0900

    Add Queue header

---
+++ Queue.h
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+class Queue
+{
+private:
+    int size;
+    int rear;
+    int front;
+    int* p;
+
+public:
+    Queue(int s);
+    void push(int a);
+    int pop();
+    bool isFull();
+    bool isEmpty();
+    void printAll();
+    void reverseQueue();
+    int topNum();
+    int countNum();
+};
