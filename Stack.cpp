b672a8f5f1e255035d07ae02ef309ec6ac04868a
dongha <donghak937@gmail.com>
2025-04-18 16:14:09 +0900

    Finish Stack

---
+++ Stack.cpp
@@ -0,0 +1,52 @@
+#include "Stack.h"
+
+Stack::Stack(int s){
+    size = s;
+    p = new int[s];
+    top = 0;
+}
+
+void Stack::push(int a){
+    if (isFull()){
+        cout << "Stack is full!" << endl;
+        return;
+    } 
+    p[top] = a;
+    top++;
+}
+
+int Stack::pop(){
+    if (isEmpty()){
+        cout << "Stack is Empty!" << endl;
+        return -1;
+    }
+
+    top--;
+    return p[top];
+}
+
+bool Stack::isFull(){
+    return top == size ? true : false;
+}
+bool Stack::isEmpty(){
+    return top == 0 ? true : false;
+}
+void Stack::printAll(){
+    for(int i = 0; i < top; i++){
+        cout << i+1 << " : " << p[top] << endl;
+    }
+}
+void Stack::reverseStack(){
+    int* d = new int[size];
+    for(int i = 0; i < top; i++){
+        d[i] = pop();
+    }
+
+    for(int i = 0; i < top; i++){
+        push(d[i]);
+    }
+    delete d;
+}
+
+
+
